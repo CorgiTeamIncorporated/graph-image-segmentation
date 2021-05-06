77e691523866c7e1aa332270ebc5b2dcc070c0a6
Leon Useinov <leon-1402@mail.ru>
2021-05-06 04:46:31 +0300

    update push_relabel test

--- tests/test_01_push_relabel.py
+++ tests/test_01_push_relabel.py
@@ -1,8 +1,10 @@
 import networkx as nx
+import random
+from networkx.algorithms.flow import preflow_push
 from push_relabel import get_max_flow
 
 
-def test_1():
+def test_1_default_input():
     graph = nx.DiGraph()
 
     graph.add_nodes_from(list(range(1, 5)))
@@ -12,4 +14,25 @@ def test_1():
     graph.add_edge(3, 4, capacity=10000)
     graph.add_edge(2, 4, capacity=10000)
 
+    assert get_max_flow(graph, 1, 4, -1) == 20000
+    assert get_max_flow(graph, 1, 4, 0) == 20000
+    assert get_max_flow(graph, 1, 4, 1) == 20000
     assert get_max_flow(graph, 1, 4, 2) == 20000
+    assert get_max_flow(graph, 1, 4, 3) == 20000
+
+
+def test_2_brute_force_input():
+    for seed in range(0, 100):
+        for size in range(2, 100):
+            graph = nx.scale_free_graph(n=size, alpha=0.15,
+                                        beta=0.7, gamma=0.15, seed=seed)
+            graph = nx.DiGraph(graph)
+
+            dict_of_weights = {'capacity': random.random()}
+            dict_of_edges_weights = {e:  dict_of_weights for e in graph.edges}
+            nx.set_edge_attributes(graph, dict_of_edges_weights)
+
+            true_max_flow = preflow_push(graph, 0, size-1, value_only=True)
+            out_max_flow = get_max_flow(graph, 0, size-1, 3)
+
+            assert true_max_flow.graph['flow_value'] == out_max_flow
