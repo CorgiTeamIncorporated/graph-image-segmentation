64b31c38140b219a708fb019639d1bd3108da012
German Ritter <ritter.german@yandex.ru>
2021-05-12 02:37:57 +0300

    minor fix

--- tests/test_01_push_relabel.py
+++ tests/test_01_push_relabel.py
@@ -10,7 +10,7 @@ def read_graph_from_file(file_path: str) -> nx.DiGraph:
 
     with open(file_path, 'r') as f:
         init_input = f.readline().split()
-        nodes_quantity, edges_quantity = list(map(int, init_input))
+        nodes_quantity, _ = list(map(int, init_input))
 
         graph.add_nodes_from(list(range(1, nodes_quantity + 1)))
 
