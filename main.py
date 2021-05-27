edcd724b10670e237177c2612d48cac87039d749
EvilKitte <47978804+EvilKitte@users.noreply.github.com>
2021-05-27 22:08:47 +0300

    GUI implemented

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Frame, Label, ttk
+from tkinter import Frame, Label, Radiobutton, StringVar, ttk, messagebox
 from tkinter import filedialog as fd
 from tkinter.constants import *
 from PIL import Image, ImageTk
@@ -10,7 +10,6 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        #self.is_first_click = True
         self.count_click = 0
         self.pack()
         self.create_top_panel()
@@ -21,12 +20,9 @@ class Application(tk.Frame):
         open_button = ttk.Button(self.f_top, text = 'Open a File',command = self.select_file)
         self.text_label = Label(text = 'Click on the object')
         self.text_label.place(x = 100, y = 6)
-        #self.text_label
         self.f_top.place(x = 5, y = 5)
         open_button.pack(expand = True)
 
-    ''' def say_hi(self, event):
-        print("hi there, everyone!", event.x, event.y)'''
     
     def select_file(self):
         filetypes = (
@@ -42,8 +38,6 @@ class Application(tk.Frame):
             initialdir = '/',
             filetypes = filetypes)
 
-        print(filename)
-
         if hasattr(self, 'photo_label'):
             self.photo_label.destroy()
         self.place_image(filename)
@@ -58,10 +52,7 @@ class Application(tk.Frame):
         self.photo_label.place (x = 10 , y = 50)
         self.photo_label.bind('<ButtonRelease>', self.click_image)
 
-        self.place_mask(image)
-
     def place_mask(self, image = None):
-        print('place_mask')
         self.mask = ImageTk.PhotoImage (image) 
 
         self.mask_label = tk.Label (image = self.mask, height = self.mask.height(), width = self.mask.width()) 
@@ -70,27 +61,47 @@ class Application(tk.Frame):
 
     def click_image(self, event):
         self.count_click += 1
+
         if self.count_click == 1:
-            print("tik1 ", event.x, ' ', event.y)
-            self.text_label.text = 'Click on the background'
-        if self.count_click == 2:
-            print("tik2 ", event.x, ' ', event.y)
+            self.first_coord_obj = self.choice_pixels(event.x, event.y, 10)
             self.text_label.destroy()
-            #вызвать создание радиобаттон
+            self.text_label = Label(text = 'Click on the background')
+            self.text_label.place(x = 100, y = 6)
+
+        if self.count_click == 2:
+            self.first_coord_bg = self.choice_pixels(event.x, event.y, 10)
+            self.text_label.destroy()
+            self.create_radiobutton()
             #отправить в сегментацию
+            #mask_image = 'функция сигментации'(self.photo, self.first_coord_obj, self.first_coord_bg)            
+            #self.mask_label.destroy()
+            #self.place_mask(mask_image)
+
         if self.count_click > 2:
-            print("tik3 ", event.x, ' ', event.y)
+            coord = self.choice_pixels(event.x, event.y, 1)
 
-        
-        
-        '''if self.is_first_click:
-            #self.text_label = Label(self.f_top, text = 'Click on the object')
-            #self.text_label.place(x = 100, y = 6)
-            print("tik ", event.x, ' ', event.y)
-            self.text_label.text = 'Click on the background'
-            print("tik ", event.x, ' ', event.y)'''
+            if self.point_type.get() != 'Object' and self.point_type.get() != 'Background':
+                messagebox.showinfo("Attention", "Select Object or Background")
+            else:
+                print("")
+                #отправить в сегментацию
+                #mask_image = 'функция сигментации'(self.photo, coord, self.point_type.get())            
+                #self.mask_label.destroy()
+                #self.place_mask(mask_image)
 
+    def create_radiobutton(self):
+        self.point_type = StringVar()
+        r1 = Radiobutton(text = 'Object', variable = self.point_type, value = 'Object')
+        r2 = Radiobutton(text = 'Background',variable = self.point_type, value = 'Background')
+        r1.place(x = 100, y = 6)
+        r2.place(x = 100, y = 25)
 
+    def choice_pixels(self, place_x, place_y, radius):
+        result = set()
+        for x in range(place_x - radius, place_x + radius + 1):
+            for y in range(place_y - radius, place_y + radius + 1):
+                result.add((x,y))
+        return(result)
 
 
 root = tk.Tk()
@@ -100,43 +111,3 @@ root.geometry('1500x750')
 
 app = Application(master=root)
 app.mainloop()
-
-
-def create_label():
-    label1 = tk.Label (height = 700, width = 700) 
-    label1.place (x = 0 , y = 50)
-    label1.config(background='red')
-
-    label2 = tk.Label (height = 700, width = 700)
-    label2.place (x = 700 , y = 50)
-    label2.config(background='green')
-
-
-
-    ''' root.update()
-    
-    factor = min(label1.winfo_width()/photo.width(), label1.winfo_height()/photo.height())
-    print(factor)
-
-    image = image1.resize ((math.ceil(factor*photo.width()), math.ceil(factor*photo.height())))
-    photo = ImageTk.PhotoImage (image)  
-    label1 = tk.Label (image = photo, height = 500, width = 1000) 
-    label1.image = photo
-    label1.place (x = 0 , y = 0)'''
-
-
-
-
-#root.update()
-#f_top = Frame(root, height = 50, width=root.winfo_width())
-#f_top.config(background='red')
-#open_button = ttk.Button( f_top, text = 'Open a File',command = select_file)
-
-#f_top.place(x = 5, y = 5)
-#open_button.pack(expand = True)
-
-#create_label()
-
-
-
-#root.mainloop()
