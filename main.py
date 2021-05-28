b527d32cf83c1ac9513956a025066c5878a25b57
exp101t <ritter.german@yandex.ru>
2021-05-28 19:50:47 +0300

    minor enhancement

--- main.py
+++ main.py
@@ -20,18 +20,42 @@ class SegmentatorGui(tk.Frame):
 
         self.pack(fill=tk.BOTH, expand=1)
 
+        # TODO: implement radius changing with slider
+        # Radius of brush
         self.radius = 5
 
-        self.offset_x = 10
-        self.offset_y = 10
+        # Defines canvas border
+        self.offset_x = 0
+        self.offset_y = 0
 
+        # Defines which brush is currently selected
         self._point_type = StringVar()
         self._point_type.set('object')
 
+        # Initial phase is phase when both
+        # background and object are needed to be marked
+        # Main phase follows initial step
+        self._phase = 'initial'
+
+        # Only needed during initial phase
+        self._is_object_selected = False
+        self._is_background_selected = False
+
+        # Colors of brushes
         self._colors = {
             'object': (255, 0, 0, 100),
             'background': (0, 0, 255, 100)
         }
+
+        # Colors of mask
+        self._mask_colors = {
+            'object': 1,
+            'background': 2
+        }
+
+        # Mask of background and object points
+        # Selected during one iteration
+        # This is not presented to user
 
         # TODO: Load image interactively
         filename = './static/banana1-gr-320.jpg'
@@ -41,6 +65,8 @@ class SegmentatorGui(tk.Frame):
         self._init_ui()
         self._update_displayed_image()
         self._init_binds()
+
+        self._mask = Image.new('L', self.orig_image.size, 0)
 
     def _init_toolbox(self):
         self._toolbox_frame = tk.LabelFrame(
@@ -81,11 +107,14 @@ class SegmentatorGui(tk.Frame):
             self,
             text='Canvas',
             relief=tk.RIDGE,
-            padx=10
+            padx=10,
+            pady=10
         )
         self._canvas_frame.grid(row=1, column=2, sticky=tk.NS)
 
-        self.canvas = tk.Canvas(self._canvas_frame, width=500, height=500)
+        self.canvas = tk.Canvas(self._canvas_frame,
+                                width=self.orig_image.size[0],
+                                height=self.orig_image.size[1])
         self.canvas.grid(row=1, column=2)
 
     def _init_ui(self):
@@ -97,6 +126,7 @@ class SegmentatorGui(tk.Frame):
     def _init_binds(self):
         self.canvas.bind('<Motion>', self.cursor_motion_handler)
         self.canvas.bind('<B1-Motion>', self.pressed_cursor_motion_handler)
+        self.canvas.bind('<ButtonRelease-1>', self.cursor_release_handler)
 
     def _update_displayed_image(self):
         self.combined_image = self.orig_image.copy()
@@ -134,20 +164,43 @@ class SegmentatorGui(tk.Frame):
         self._update_displayed_image()
 
     def pressed_cursor_motion_handler(self, event):
+        # Clear cursor before drawing
         self.pointer_image = Image.new('RGBA', self.orig_image.size)
-        ImageDraw.Draw(self.mask_image).ellipse(
-            get_circle_bounding_box(event.x - self.offset_x,
-                                    event.y - self.offset_y,
-                                    self.radius),
-            fill=self._colors[self._point_type.get()]
+
+        box = get_circle_bounding_box(event.x - self.offset_x,
+                                      event.y - self.offset_y,
+                                      self.radius)
+
+        ImageDraw.Draw(self._mask).ellipse(
+            box, fill=self._mask_colors[self._point_type.get()]
         )
+        ImageDraw.Draw(self.mask_image).ellipse(
+            box, fill=self._colors[self._point_type.get()]
+        )
+
         self._update_displayed_image()
 
+    def cursor_release_handler(self, _):
+        if self._phase == 'initial':
+            point_type_str = self._point_type.get()
+            self._is_object_selected |= (point_type_str == 'object')
+            self._is_background_selected |= (point_type_str == 'background')
 
-root = tk.Tk()
-root.title('Graph Image Segmentation')
-root.resizable(False, False)
-root.geometry('800x600')
+            if self._is_object_selected and self._is_background_selected:
+                # Send mask to segmentator
+                self._phase = 'main'
+                pass
+        else:
+            # Send mask to segmentator
+            self._mask = Image.new('L', self.orig_image.size, 0)
+            pass
 
-app = SegmentatorGui(master=root)
-app.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Graph Image Segmentation')
+    root.resizable(False, False)
+    root.geometry('800x600')
+
+    app = SegmentatorGui(master=root)
+    app.mainloop()
